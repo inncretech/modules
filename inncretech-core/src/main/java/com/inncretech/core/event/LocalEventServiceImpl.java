72d9051901610fd41cd9854aecfba9fa403be42b
Pranab Dash <pranabd@gmail.com>
2013-05-26 20:53:42 +0530

    mudules checkin

--- inncretech-core/src/main/java/com/inncretech/core/event/LocalEventServiceImpl.java
+++ inncretech-core/src/main/java/com/inncretech/core/event/LocalEventServiceImpl.java
@@ -1,5 +1,19 @@
 package com.inncretech.core.event;
 
+import com.inncretech.core.event.model.Event;
+
 public class LocalEventServiceImpl implements EventServiceImpl{
+
+  @Override
+  public void registerEventHandler(Event event, EventHandler handler) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void publishEvent(Event event) {
+    // TODO Auto-generated method stub
+    
+  }
 
 }
