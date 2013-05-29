392b14004314c4b7d429be6d5f19dda7d9a16512
Pranab Dash <pranabd@gmail.com>
2013-05-29 22:43:14 +0530

    comment service changed

--- inncretech-comment/src/main/java/com/inncretech/comment/service/CommentService.java
+++ inncretech-comment/src/main/java/com/inncretech/comment/service/CommentService.java
@@ -2,7 +2,7 @@ package com.inncretech.comment.service;
 
 public interface CommentService {
 
-  Object create(Long sourceId, Object commentDto);
+  Object create(Long sourceId, String comment);
   Object getAllComments(Long sourceId);
   
 }
