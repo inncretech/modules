0761a94693f881026233eb0197d603ae11a70a32
Pranab Dash <ranabd@gmail.com>
2013-06-13 00:04:39 +0530

    Mahesh changes

--- inncretech-like-service/src/test/java/com/inncretech/like/DefaultLikeServiceImplTest.java
+++ inncretech-like-service/src/test/java/com/inncretech/like/DefaultLikeServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.inncretech.like;
 
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +9,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.inncretech.core.model.AccessContext;
+
 import com.inncretech.core.test.TestUtil;
+
+import com.inncretech.like.dao.SourceLikeDao;
 import com.inncretech.like.service.LikeService;
 
 
@@ -20,18 +24,18 @@ public class DefaultLikeServiceImplTest {
   private LikeService likeService;
   @Autowired
   private TestUtil dbUtility;
-
+  @Autowired
+  SourceLikeDao objDDAO ;
   
   @Test
   public void LikeSource() {
  
-    likeService.likeSource((long) 2,(long) 3,new AccessContext());
+    long objID= objDDAO.getIdGenService().getNewSourceId();
+    long usrID =objDDAO.getIdGenService().getNewUserId();
+    likeService.likeSource(objID,usrID,new AccessContext());
     
   }
-  @Before
-  public void setUp() {
-    dbUtility.cleanUpdb();
 
-  }
+  
 }
 
