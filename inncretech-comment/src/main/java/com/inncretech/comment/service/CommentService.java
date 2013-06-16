0b84b21e9a4fc83992b8ad01c49022b6be51c928
Gaurav <gaurav@gaurav-computer.(none)>
2013-06-16 19:20:29 +0530

    updating CommentService

--- inncretech-comment/src/main/java/com/inncretech/comment/service/CommentService.java
+++ inncretech-comment/src/main/java/com/inncretech/comment/service/CommentService.java
@@ -3,16 +3,11 @@ package com.inncretech.comment.service;
 import java.util.List;
 
 import com.inncretech.comment.model.Comment;
-import com.inncretech.core.model.AccessContext;
+
 
 public interface CommentService {
 
-<<<<<<< HEAD
-  public Comment create(Long sourceId, Comment comment,AccessContext accessContext);
+  public Comment create(Long sourceId, Comment comment);
   public List<Comment> getAllComments(Long sourceId);
-=======
-  Object create(Long sourceId, String comment);
-  Object getAllComments(Long sourceId);
->>>>>>> 3bb2c5afddc8fc2cb649ca7bc18a120a97589047
   
 }
