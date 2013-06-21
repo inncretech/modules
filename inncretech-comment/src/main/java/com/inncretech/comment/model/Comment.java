5e463d4d7ba92df7cde2d21afaa91dee3581dd2c
Pranab Dash <pranabd@gmail.com>
2013-06-21 21:34:29 +0530

    comment checkin

--- inncretech-comment/src/main/java/com/inncretech/comment/model/Comment.java
+++ inncretech-comment/src/main/java/com/inncretech/comment/model/Comment.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import com.inncretech.core.model.ShardEntity;
