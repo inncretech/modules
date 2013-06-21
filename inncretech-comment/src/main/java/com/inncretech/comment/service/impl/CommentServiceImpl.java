c9287991ff27b9fe59f5db860664535855a6ed06
Pranab Dash <pranabd@gmail.com>
2013-06-21 22:32:25 +0530

    comment test case

--- inncretech-comment/src/main/java/com/inncretech/comment/service/impl/CommentServiceImpl.java
+++ inncretech-comment/src/main/java/com/inncretech/comment/service/impl/CommentServiceImpl.java
@@ -2,7 +2,9 @@ package com.inncretech.comment.service.impl;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,20 +28,26 @@ public class CommentServiceImpl implements CommentService {
 	@Override
 	public List<Comment> getAllComments(Long sourceId) {
 		List<Comment> listComment = commentDao.getComments(sourceId);
-
-		List<Comment> commentObjWithChildList = new ArrayList<Comment>();
-		for (Comment tempComment1 : listComment) {
-			Comment comment = new Comment();
-			List<Comment> childNode = new ArrayList<Comment>();
-			for (Comment tempComment2 : listComment) {
-				if (tempComment1.getId() == tempComment2.getCommentParentId()) {
-					childNode.add(tempComment2);
-				}
-			}
-			comment = tempComment1;
-			comment.setChildComments(childNode);
-			commentObjWithChildList.add(comment);
+		Map<Long, Comment> commentMap = new HashMap<Long, Comment>();
+		for(Comment comment : listComment){
+		  if(comment.getCommentParentId() !=null && !commentMap.containsKey(comment.getCommentParentId())){
+		    throw new RuntimeException("THis conidtion should not happen");
+		  }
+		  if(comment.getCommentParentId() !=null ){
+        Comment parentComment = commentMap.get(comment.getCommentParentId());
+        parentComment.getChildComments().add(comment);
+        commentMap.put(comment.getId(), comment);
+      }else{
+        commentMap.put(comment.getId(), comment);
+      }
+		  
 		}
-		return commentObjWithChildList;
+		List<Comment> firstLevelComments = new ArrayList<Comment>();
+		for(Long commentId : commentMap.keySet())
+		{
+		  if(commentMap.get(commentId).getCommentParentId() == null)
+		    firstLevelComments.add(commentMap.get(commentId));
+		}
+		return firstLevelComments;
 	}
 }
