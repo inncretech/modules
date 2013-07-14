e0ca23d344c15526f56c4728d0c5216ce885b391
Gaurav <gaurav@gaurav-computer.(none)>
2013-07-14 21:12:54 +0530

    adding code for share via facebook

--- inncretech-social/src/main/java/com/inncretech/social/service/impl/SocialServiceImpl.java
+++ inncretech-social/src/main/java/com/inncretech/social/service/impl/SocialServiceImpl.java
@@ -1,25 +1,33 @@
 package com.inncretech.social.service.impl;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import com.inncretech.social.service.SocialService;
+import com.inncretech.social.service.constants.SocialServiceConstants;
 
 @Service
 public class SocialServiceImpl implements SocialService {
-
+	//private String facebookGraphUrl="https://graph.facebook.com/";
+	
 	@Override
 	public void sharePostToFacebook(String facebookId, String accessToken,
-			Map<String, String> postData) {
-		// TODO Auto-generated method stub
+			MultiValueMap<String, String> postData) {
+		if (facebookId != null) {
+			postData.add("access_token", accessToken);
+			String facebookFeedUrl = SocialServiceConstants.FB_GRAPH_URL + "/me/feed";
+			new RestTemplate().postForObject(facebookFeedUrl, postData,
+					String.class);
+		}
 		
 	}
-
+	
 	@Override
 	public void sharePostToTwitter(String twitterId) {
 		// TODO Auto-generated method stub
 		
 	}
+
 	
 }
