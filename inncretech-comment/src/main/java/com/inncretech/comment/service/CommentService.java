5ee07a32bdf91bb89ff4e935baa3243df06fb273
Pranab Dash <pranabd@gmail.com>
2013-08-15 20:13:21 +0530

    comment changes

--- inncretech-comment/src/main/java/com/inncretech/comment/service/CommentService.java
+++ inncretech-comment/src/main/java/com/inncretech/comment/service/CommentService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.inncretech.comment.model.Comment;
 
 public interface CommentService {
-	public Comment create(Long sourceId, Comment comment);
+	public Comment create(Long sourceId, String commentText, Long createdBy, Long parentCommentId);
 	public List<Comment> getAllComments(Long sourceId);
 
 }
