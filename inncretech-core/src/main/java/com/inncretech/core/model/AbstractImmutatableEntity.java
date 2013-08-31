dd0d0ff62a3b509a64546233378fb57afa6bf6e5
deepaks <deepaks@gmail.com>
2013-08-30 18:31:55 -0700

    Basic classes for any domain model to extend or implement.

---
+++ inncretech-core/src/main/java/com/inncretech/core/model/AbstractImmutatableEntity.java
@@ -0,0 +1,98 @@
+package com.inncretech.core.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+
+@MappedSuperclass
+public class AbstractImmutatableEntity extends BaseEntity {
+
+  private DateTime createdAt;
+
+  private Long createdBy;
+
+  /**
+   * Return the value associated with the column: createdAt.
+   * 
+   * @return A DateTime object (this.createdAt)
+   */
+  @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+  @Basic(optional = true)
+  @Column(name = "created_at")
+  public DateTime getCreatedAt() {
+    return this.createdAt;
+
+  }
+
+  /**
+   * Set the value related to the column: createdAt.
+   * 
+   * @param createdAt
+   *          the createdAt value you wish to set
+   */
+  public void setCreatedAt(final DateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  /**
+   * Return the value associated with the column: createdBy.
+   * 
+   * @return A Long object (this.createdBy)
+   */
+  @Basic(optional = true)
+  @Column(name = "created_by")
+  public Long getCreatedBy() {
+    return this.createdBy;
+
+  }
+
+  /**
+   * Set the value related to the column: createdBy.
+   * 
+   * @param createdBy
+   *          the createdBy value you wish to set
+   */
+  public void setCreatedBy(final Long createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+    result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!super.equals(obj))
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    AbstractImmutatableEntity other = (AbstractImmutatableEntity) obj;
+    if (createdAt == null) {
+      if (other.createdAt != null)
+        return false;
+    } else if (!createdAt.equals(other.createdAt))
+      return false;
+    if (createdBy == null) {
+      if (other.createdBy != null)
+        return false;
+    } else if (!createdBy.equals(other.createdBy))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return super.toString() + " [AbstractImmutatableEntity [createdAt=" + createdAt + ", createdBy=" + createdBy;
+  }
+}
---
+++ inncretech-core/src/main/java/com/inncretech/core/model/BaseEntity.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.inncretech.core.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+
+/**
+ * Base class to derive entity classes from.
+ * 
+ * @author shade05
+ */
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+  private Long id;
+
+  /**
+   * Returns the identifier of the entity.
+   * 
+   * @return the id
+   */
+  @Id
+  @Basic(optional = false)
+  @Column(name = "id", nullable = false)
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    BaseEntity other = (BaseEntity) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "BaseEntity [id=" + id + "]";
+  }
+
+  /**
+   * Return the type of this class. Useful for when dealing with proxies.
+   * 
+   * @return Defining class.
+   */
+  @Transient
+  public Class<?> getClassType() {
+    return this.getClass();
+  }
+}
