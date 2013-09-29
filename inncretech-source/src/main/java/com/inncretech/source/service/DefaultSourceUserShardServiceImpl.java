73c7398e536d4cc09a32551e12c89c4cd4cedbc6
deepaks <deepaks@gmail.com>
2013-09-28 17:17:54 -0700

    Changes related to Magazine Source which would be colocated with User table.

--- inncretech-source/src/main/java/com/inncretech/source/service/DefaultSourceUserShardServiceImpl.java
+++ inncretech-source/src/main/java/com/inncretech/source/service/DefaultSourceUserShardServiceImpl.java
@@ -6,33 +6,33 @@ import org.springframework.stereotype.Service;
 import com.inncretech.core.sharding.IdGenerator;
 import com.inncretech.core.sharding.ShardAware;
 import com.inncretech.core.sharding.ShardType;
-import com.inncretech.source.dao.SourceDao;
+import com.inncretech.source.dao.SourceUserShardDao;
 import com.inncretech.source.model.Source;
 
 @Service
-public class DefaultSourceServiceImpl implements SourceService {
+public class DefaultSourceUserShardServiceImpl implements SourceUserShardService {
 
   @Autowired
-  private SourceDao sourceDao;
+  private SourceUserShardDao sourceUserShardDao;
 
   @Autowired
   private IdGenerator idGenerator;
 
   @Override
-  @ShardAware(shardStrategy = "entityid", shardType = ShardType.SOURCE)
+  @ShardAware(shardStrategy = "entityid", shardType = ShardType.USER)
   public Source create(Source source) {
-    return sourceDao.createSource(source);
+    return sourceUserShardDao.createSource(source);
   }
 
   @Override
-  @ShardAware(shardStrategy = "entityid", shardType = ShardType.SOURCE)
+  @ShardAware(shardStrategy="entityid", shardType= ShardType.USER)
   public void delete(Long sourceId) {
 
   }
 
   @Override
-  @ShardAware(shardStrategy = "entityid", shardType = ShardType.SOURCE)
+  @ShardAware(shardStrategy="entityid", shardType= ShardType.USER)
   public Source get(Long sourceId) {
-    return sourceDao.get(sourceId);
+    return sourceUserShardDao.get(sourceId);
   }
 }
--- inncretech-source/src/main/java/com/inncretech/source/service/SourceUserShardService.java
+++ inncretech-source/src/main/java/com/inncretech/source/service/SourceUserShardService.java
@@ -4,15 +4,15 @@ import com.inncretech.core.sharding.ShardAware;
 import com.inncretech.core.sharding.ShardType;
 import com.inncretech.source.model.Source;
 
-public interface SourceService {
+public interface SourceUserShardService {
 
-  @ShardAware(shardStrategy="entityid", shardType= ShardType.SOURCE)
+  @ShardAware(shardStrategy="entityid", shardType= ShardType.USER)
   Source create(Source source);
   
-  @ShardAware(shardStrategy="entityid", shardType= ShardType.SOURCE)
+  @ShardAware(shardStrategy="entityid", shardType= ShardType.USER)
   void delete(Long sourceId);
   
-  @ShardAware(shardStrategy="entityid", shardType= ShardType.SOURCE)
+  @ShardAware(shardStrategy="entityid", shardType= ShardType.USER)
   Source get(Long sourceId);
 
 }
