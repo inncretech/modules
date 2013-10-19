f736791c8a28a6a7d57a142e6ea5808cdde06669
deepaks <deepaks@gmail.com>
2013-10-19 16:18:19 -0700

    Missed file in the last checkin

---
+++ inncretech-notification/src/main/java/com/inncretech/notification/dao/impl/NotificationDaoImpl.java
@@ -0,0 +1,16 @@
+package com.inncretech.notification.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.inncretech.core.sharding.dao.GenericSourceShardDaoImpl;
+import com.inncretech.notification.dao.NotificationDao;
+import com.inncretech.notification.model.Notification;
+
+@SuppressWarnings("unchecked")
+@Component
+public class NotificationDaoImpl extends GenericSourceShardDaoImpl<Notification, Long> implements NotificationDao {
+
+  public NotificationDaoImpl() {
+    super(Notification.class);
+  }
+}
