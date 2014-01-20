b27cfde4ccc40224a62a348777eb72c7d2e20a73
Erik Hayes <erik@hayess.net>
2014-01-20 13:17:44 -0500

    Add DELETED record status

--- inncretech-core/src/main/java/com/inncretech/core/model/RecordStatus.java
+++ inncretech-core/src/main/java/com/inncretech/core/model/RecordStatus.java
@@ -1,14 +1,14 @@
 package com.inncretech.core.model;
 
 public enum RecordStatus {
-  ACTIVE((byte)1) , INACTIVE((byte)0);
+  ACTIVE((byte)1) , INACTIVE((byte)0), DELETED(null);
   
-  private byte id;
-  private RecordStatus(byte id){
+  private Byte id;
+  private RecordStatus(Byte id){
     this.id = id;
   }
   
-  public byte getId(){
+  public Byte getId(){
     return this.id;
   }
 }
