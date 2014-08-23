d0cf7104b19ec94c1b1ff61b357e5144131a127e
rmmaheshwari <rmmaheshwari@yahoo.com>
2014-08-23 16:44:35 +0530

    session and cart service

---
+++ inncretech-merchant-app/bl/Cart/cartdao/src/main/java/com/inncretech/cart/db/repository/ShoppingCartItemsRepository.java
@@ -0,0 +1,13 @@
+package com.inncretech.cart.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inncretech.cart.db.beans.ShoppingCartItem;
+
+@Repository
+public interface ShoppingCartItemsRepository extends JpaRepository<ShoppingCartItem, Long> {
+
+	public ShoppingCartItem findShoppingCartItemByItemId(Long itemId);
+
+}
